469d31a9f403d4be6f8d7b22ac7cd0b179960cd8
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-06 22:41:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+<<<<<<< HEAD
 puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
+=======
+"   |   |
+ -----------
+    |   |
+ -----------
+    |   |   "
+>>>>>>> d5cfc97eac9089eadbef459e3bf0a64ac998935a
 end
